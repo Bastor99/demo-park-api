1ea2ad6601447e12da2ad134a3575f58faf4d1f4
Vitor <vitorbastos195@gmail.com>
2025-05-28 10:44:59 -0300

    Recurso criação de usuario

--- src/main/java/com/bastor99/demo_park_api/service/UsuarioService.java
+++ src/main/java/com/bastor99/demo_park_api/service/UsuarioService.java
@@ -1,8 +1,10 @@
 package com.bastor99.demo_park_api.service;
 
+import com.bastor99.demo_park_api.entity.Usuario;
 import com.bastor99.demo_park_api.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +12,8 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
 
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
