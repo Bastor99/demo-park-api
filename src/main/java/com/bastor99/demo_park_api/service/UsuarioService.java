784bc98092f79202cea065cc23fa363b90dd5ae8
Vitor <vitorbastos195@gmail.com>
2025-05-28 10:37:49 -0300

    Criação Usuario Repository, Service e Controller

---
+++ src/main/java/com/bastor99/demo_park_api/service/UsuarioService.java
@@ -0,0 +1,13 @@
+package com.bastor99.demo_park_api.service;
+
+import com.bastor99.demo_park_api.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+}
